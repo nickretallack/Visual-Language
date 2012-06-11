d9234b25bc01cac63119d73f9def45caf31983ee
Nick Retallack <nickretallack@gmail.com>
2012-06-10 19:30:32 -0700

    added dependencies

---
+++ library/uuid.js
@@ -0,0 +1,8 @@
+function UUID() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+S4()+S4()+S4()+S4()+S4()+S4());
+}
+
+if (typeof exports === 'object') exports.UUID = UUID
